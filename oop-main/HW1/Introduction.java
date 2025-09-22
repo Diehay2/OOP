0a0cf4c3e08d8b84a9fc87e7f70aad1278d8d0fc
Diehay2 <dzung.dangquang@gmail.com>
2025-09-22 20:45:49 +0700

    HW1

---
+++ oop-main/HW1/Introduction.java
@@ -0,0 +1,15 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.print("Đặng Quang Dũng\t");
+        System.out.print("24021425\t");
+        System.out.print("K69I - CS5\t");
+        System.out.print("Diehay2\t");
+        System.out.print("dzung.dangquang@gmail.com\n");
+
+        for (int i = 9; i >= 0; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+            if (i == 0) System.out.println("No more bottles of beer on the wall.");
+        }
+    }
+}
