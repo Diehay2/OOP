f6b90248a4fe5d096372b33fd061ec058302910d
Diehay2 <dzung.dangquang@gmail.com>
2025-09-23 16:43:04 +0700

    Fix

--- oop-main/HW1/Introduction.java
+++ oop-main/HW1/Introduction.java
@@ -7,9 +7,11 @@ public class Introduction {
         System.out.print("dzung.dangquang@gmail.com\n");
 
         for (int i = 9; i >= 0; i--) {
-            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
-            System.out.println("Take one down, pass it around,");
             if (i == 0) System.out.println("No more bottles of beer on the wall.");
+            else {
+                System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+                System.out.println("Take one down, pass it around,");
+            }
         }
     }
 }
